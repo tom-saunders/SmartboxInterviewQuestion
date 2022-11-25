bfa9ce32aacaaad1956894417b5e0a407954cdd8
Tom Saunders <tom@paradoxic.al>
2022-11-25 20:19:58 +0000

    Initial comments and queries from cursory scan.
    Likely missing some fairly obvious things but this was what jumped out initially.

--- InterviewQuestion/SeeTechEyeGazeCamera.cs
+++ InterviewQuestion/SeeTechEyeGazeCamera.cs
@@ -16,19 +16,33 @@ namespace InterviewQuestion
 
         public SeeTechEyeGazeCamera(ISeeTechDriver driver)
         {
+            // csharp q - can this be null?
             _driver = driver;
         }
 
         public void Start()
         {
+            // no error handling
+            // this blocks? should we handle this async and allow other tasks to progress?
             _driver.Connect();
+
+            // starts even if we didn't connect
+            // can/should this be created here or in ctor? can it fail?
             _cancellation = new CancellationTokenSource();
+
+            // csharp q - is this a busy loop?
+            //     see Loop method below - not part of the language itself
+            // should it be waiting on an event and then checking whether to reschedule?
             Task.Run(() => Loop(_cancellation.Token));
         }
 
         public void Stop()
         {
+            // no error handling
+            // if we failed Connect() then do we fail expectations calling Disconnect()?
             _driver.Disconnect();
+            
+            // if we fail disconnect what state do we get left in? are we leaking a resource?
             _cancellation.Cancel();
         }
 
@@ -36,7 +50,13 @@ namespace InterviewQuestion
         {
             while (!token.IsCancellationRequested)
             {
-                int[] frame = new int[6]; _driver.GetNextFrame(frame, -1);
+                // this allocates a new array for each frame?
+                // stub interface implies 20/sec, this is allocating 480 bytes per second (doubled or tripled with p/r/l/parameter to FrameChanged?)
+                // csharp q - presumably new allocates on the heap and will then be GCd? performance hit from fragmented short lived allocations?
+                int[] frame = new int[6];
+
+                // this will potentially block forever
+                _driver.GetNextFrame(frame, -1);
                 var p = new Point(frame[0], frame[1]);
                 var r = new Point(frame[2], frame[3]);
                 var l = new Point(frame[4], frame[5]);
--- InterviewQuestion/SeeTechFrame.cs
+++ InterviewQuestion/SeeTechFrame.cs
@@ -7,6 +7,7 @@ namespace InterviewQuestion
     /// </summary>
     public class SeeTechFrame : IFrame
     {
+        // base interface is nonmutable - is this hiding the base or extending it?
         public Point GazePosition { get; set; }
         public Point LeftEye { get; set; }
         public Point RightEye { get; set; }
